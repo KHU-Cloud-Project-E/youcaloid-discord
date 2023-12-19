b32fde4c7a76f72ec2016b672c65021682b155b6
kwakkyoleen <2007kkl@naver.com>
2023-12-20 02:00:36 +0900

    Update DataBaseService.java

--- src/main/java/com/noeun/youcaloid/db/DataBaseService.java
+++ src/main/java/com/noeun/youcaloid/db/DataBaseService.java
@@ -68,43 +68,43 @@ public class DataBaseService {
 
     public String getModelId(String guildId, String userId){
         return jdbcTemplate.queryForList(
-            String.format("SELECT MODELID FROM user_info WHERE GUILDID = %s AND USERID = %s",guildId,userId)
+            String.format("SELECT MODELID FROM USER_INFO WHERE GUILDID = %s AND USERID = %s",guildId,userId)
             , String.class).get(0);
     }
 
     public int addModelId(String guildId, String userId, String modelId){
         if (jdbcTemplate.queryForList(
-            String.format("SELECT MODELID FROM user_info WHERE GUILDID = %s AND USERID = %s",guildId,userId)
+            String.format("SELECT MODELID FROM USER_INFO WHERE GUILDID = %s AND USERID = %s",guildId,userId)
             , String.class).size() == 0){
-        return jdbcTemplate.update("INSERT INTO user_info (GUILDID, USERID, MODELID) VALUES (?, ?, ?)",guildId,userId,modelId);
+        return jdbcTemplate.update("INSERT INTO USER_INFO (GUILDID, USERID, MODELID) VALUES (?, ?, ?)",guildId,userId,modelId);
             }else{
-                return jdbcTemplate.update("UPDATE user_info SET MODELID = ? WHERE GUILDID = ? AND USERID = ? ",modelId,guildId,userId);
+                return jdbcTemplate.update("UPDATE USER_INFO SET MODELID = ? WHERE GUILDID = ? AND USERID = ? ",modelId,guildId,userId);
             }
     }
 
     public String nowModel(String guildId, String userId){
         return jdbcTemplate.queryForList(
-            String.format("SELECT model_info.NAME FROM user_info INNER JOIN model_info ON user_info.MODELID = model_info.MODELID WHERE GUILDID = %s AND USERID = %s",guildId,userId)
+            String.format("SELECT MODEL_INFO.NAME FROM USER_INFO INNER JOIN MODEL_INFO ON USER_INFO.MODELID = MODEL_INFO.MODELID WHERE GUILDID = %s AND USERID = %s",guildId,userId)
             , String.class).get(0);
     }
 
     public String getModelDec(String modelId){
          return jdbcTemplate.queryForList(
-            String.format("SELECT NAME FROM model_info WHERE MODELID = %s", modelId)
+            String.format("SELECT NAME FROM MODEL_INFO WHERE MODELID = %s", modelId)
             , String.class).get(0);
     }
 
     public int setMacro(String userId, int macroNum, String modelId){
         if(jdbcTemplate.queryForList(
             String.format(
-                "SELECT MODELID FROM model_info WHERE MODELID = %s", 
+                "SELECT MODELID FROM MODEL_INFO WHERE MODELID = %s", 
                 modelId), String.class).size() > 0
         ){
-            if(jdbcTemplate.queryForList(String.format("SELECT MODELID FROM macro WHERE USERID = %s AND MACRONUM = %s", 
+            if(jdbcTemplate.queryForList(String.format("SELECT MODELID FROM MACRO WHERE USERID = %s AND MACRONUM = %s", 
                 userId, String.valueOf(macroNum)), String.class).size() > 0){
-                    return jdbcTemplate.update("UPDATE  macro SET MODELID = ? WHERE USERID = ? AND MACRONUM = ?", modelId, userId, String.valueOf(macroNum));
+                    return jdbcTemplate.update("UPDATE  MACRO SET MODELID = ? WHERE USERID = ? AND MACRONUM = ?", modelId, userId, String.valueOf(macroNum));
                 }else{
-                    return jdbcTemplate.update("INSERT INTO macro (USERID, MACRONUM, MODELID) VALUES (?, ?, ?)",userId, String.valueOf(macroNum), modelId);
+                    return jdbcTemplate.update("INSERT INTO MACRO (USERID, MACRONUM, MODELID) VALUES (?, ?, ?)",userId, String.valueOf(macroNum), modelId);
                 }
         }
 
@@ -113,10 +113,10 @@ public class DataBaseService {
 
     public int changeModel(String guildId, String userId, int macroNum){
         if(jdbcTemplate.queryForList(String.format(
-            "SELECT MODELID FROM macro WHERE USERID = %s AND MACRONUM = %s", 
+            "SELECT MODELID FROM MACRO WHERE USERID = %s AND MACRONUM = %s", 
             userId, String.valueOf(macroNum)), String.class).size() > 0){
                 String modelId = jdbcTemplate.queryForList(String.format(
-            "SELECT MODELID FROM macro WHERE USERID = %s AND MACRONUM = %s", 
+            "SELECT MODELID FROM MACRO WHERE USERID = %s AND MACRONUM = %s", 
             userId, String.valueOf(macroNum)), String.class).get(0);
             return addModelId(guildId, userId, modelId);
         }
@@ -125,13 +125,13 @@ public class DataBaseService {
 
     public String getMacro(String userId){
         String rString = "";
-        List<MacroType> macros = jdbcTemplate.query(String.format("SELECT macro.MACRONUM, model_info.NAME FROM macro INNER JOIN model_info ON macro.MODELID = model_info.MODELID WHERE macro.USERID = %s", userId),
+        List<MacroType> macros = jdbcTemplate.query(String.format("SELECT MACRO.MACRONUM, MODEL_INFO.NAME FROM MACRO INNER JOIN MODEL_INFO ON MACRO.MODELID = MODEL_INFO.MODELID WHERE MACRO.USERID = %s", userId),
         new RowMapper<MacroType>(){
             @Override
             public MacroType mapRow(ResultSet rs, int rowNum) throws SQLException {
                 MacroType macroType = new MacroType();
-                macroType.setMacroNum(rs.getString("macro.MACRONUM"));
-                macroType.setModelDec(rs.getString("model_info.NAME"));
+                macroType.setMacroNum(rs.getString("MACRO.MACRONUM"));
+                macroType.setModelDec(rs.getString("MODEL_INFO.NAME"));
                 return macroType;
             }
         });
